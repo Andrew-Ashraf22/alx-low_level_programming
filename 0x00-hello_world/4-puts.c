bb34b09f8dac5a6cb322e97ddef19585105b1752
Andrew <androa45@yahoo.com>
2023-03-16 17:59:42 -0700

    commited

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - the starting point
+*return: 0 means (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
