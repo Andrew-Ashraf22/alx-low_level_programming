cdd8a6306a86730dbbf74a82d6a4517f24271d34
Andrew <androa45@yahoo.com>
2023-03-16 18:13:03 -0700

    commited

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+  2 /**
+  3 *main - entry point (start)
+  4 *Return: always 0 (success)
+  5 */
+  6 int main(void)
+  7 {
+  8         printf("with proper grammar, but the outcome is a piece of art,\n");
+  9         return (0);
+ 10 }
