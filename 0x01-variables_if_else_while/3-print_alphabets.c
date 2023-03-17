8bd336a8f95866c3f4fcb6d1cf28a65022b327c7
Andrew <androa45@yahoo.com>
2023-03-17 16:59:38 -0700

    0x02 c task commited

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: 0(success)
+*/
+int main(void)
+{
+	char ch;
+
+	for(ch = 'a'; ch >= 'a' && ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	for(ch = 'A'; ch >= 'A' && ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+}
