bbd2e32dbbfee99221e8a024e506fdfcfe4ef047
Andrew <androa45@yahoo.com>
2023-03-22 14:29:45 -0700

    task commited

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_abs - get the absloute value of a number
+*@n: number to be checked
+*Return: n after getting abslouted
+*/
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-1 * n);
+}
