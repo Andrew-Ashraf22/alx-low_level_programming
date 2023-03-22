f00f7e8ca4bde4a2fcb05a44842f2d5a1a3165f5
Andrew <androa45@yahoo.com>
2023-03-22 15:27:11 -0700

    task commited

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*jack_bauer - prints every min of a day
+*/
+void jack_bauer(void)
+{
+	inti, j;
+
+	i = 0;
+
+	while (i < 24)
+	{
+		j = 0;
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++
+	}
+}
