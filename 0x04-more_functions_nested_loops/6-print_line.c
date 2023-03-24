86efdf6d9c1473d51bfc174aca3d5ddbd0ff66dc
Andrew <androa45@yahoo.com>
2023-03-23 17:59:50 -0700

    C more functions and nested loops task commited

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*print_line - print n lines
+*@n: number of lines to be printed
+*/
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
