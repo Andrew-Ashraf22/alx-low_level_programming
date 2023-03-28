6c06cb9b16e0d039d6fc181a550799b835dd0a2a
Andrew <androa45@yahoo.com>
2023-03-27 19:05:00 -0700

    c-pointer,arrays task commited

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - generate a random password
+*Return: 0
+*/
+int main(void)
+{
+	int pass[100];
+	int i, sum, j;
+
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			j = 2722 - sum - '0';
+			sum += j;
+			putchar(j + '0');
+			break;
+		}
+	}
+	return (0);
+}
