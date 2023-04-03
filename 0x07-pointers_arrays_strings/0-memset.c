b12fcb6efffa4bd6ba18c456069d6102bd8dee7d
Andrew <androa45@yahoo.com>
2023-04-03 15:10:03 -0700

    C-even more pointers,array task commited

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_memset - fills memeory with a constant byte
+*@n: number of bytes 
+*@s: starting byte
+*@b: the constant byte
+*Return: a pointer to s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
