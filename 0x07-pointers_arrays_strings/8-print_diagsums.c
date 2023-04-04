cde06ad2d959d97a86e4ed8237179d9b982a9272
Andrew <androa45@yahoo.com>
2023-04-03 17:02:12 -0700

    C-even more pointers,array task commited

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*print_diagsums - sum of 2 diagonal squares
+*@a: the pointer
+*@size: the size
+*/
+void print_diagsums(int *a, int size)
+{
+	int i = 0, j = 0, k;
+
+	for (k = 0; k < size; k++)
+	{
+		i = j + a[k * size + k];
+	}
+	for (k = size - 1; k >= 0; k--)
+	{
+		j += a[k * size + (size - k - 1)];
+	}
+	printf("%d, %d\n", i, j);
+}
