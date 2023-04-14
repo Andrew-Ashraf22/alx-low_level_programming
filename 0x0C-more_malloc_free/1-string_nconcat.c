1f96d57ebd2bacfadcf60162128bb6ca23e12bf2
Andrew <androa45@yahoo.com>
2023-04-13 18:09:08 -0700

    second task 3

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,11 +15,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	unsigned int i = 0, j = 0, k = 0, m = 0;
 
-	while (s1[i] != '\0')
+	while (s1[i] != '\0' && s1)
 	{
 		i++;
 	}
-	while (s2[j] != '\0')
+	while (s2[j] != '\0' && s2)
 	{
 		j++;
 	}
