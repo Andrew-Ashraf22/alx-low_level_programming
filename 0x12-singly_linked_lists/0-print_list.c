8ee6f2f4567b84d16b5b2658caf118214dc6c054
Andrew <androa45@yahoo.com>
2023-04-27 14:07:01 -0700

    first task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+  *print_list - print elemetns
+  *@h: pointer to list
+  *Return: the size
+  */
+size_t print_list(const list_t *h)
+{
+	size_t size = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0](nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+	h = h->next;
+	size++;
+	}
+	return (size);
+}
