ddd7327522cede6109517e6f876530a7148bcfb7
Andrew <androa45@yahoo.com>
2023-04-27 15:14:24 -0700

    fifth task upd

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,6 +6,13 @@
   */
 void free_list(list_t *head)
 {
-	free(head->str);
-	free(head);
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
 }
