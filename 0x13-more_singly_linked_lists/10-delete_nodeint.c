54cc79fcfc1255a08b14adbc8da2c39a5e5bd504
Andrew <androa45@yahoo.com>
2023-05-01 09:17:09 -0700

    task 11

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+  *delete_nodeint_at_index - remove node at certin index
+  *@head: list
+  *@index: index to delete at
+  *Return: 1 if deleted -1 if failed
+  */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *temp = *head;
+	listint_t *prev = NULL;
+
+	if (index == 0)
+	{
+		*head = (temp)->next;
+		free(temp);
+		return (1);
+	}
+	while (temp && i != index)
+	{
+		prev = temp;
+		temp = temp->next;
+		i++;
+	}
+	if (temp == NULL)
+		return (-1);
+
+	prev->next = temp->next;
+	free(temp);
+	return (1);
+}
