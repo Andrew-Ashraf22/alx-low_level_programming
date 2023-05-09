0358100a1650b5a62113aa3935d6ca1073a29368
Andrew <androa45@yahoo.com>
2023-05-08 18:49:32 -0700

    task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,11 +14,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		while (text_content)
-		{
+		while (text_content[size])
 			size++;
-			text_content++;
-		}
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
