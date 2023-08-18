ed3ed77e55ad42f43c4a3df0bd220e859759e671
Andrew-Ashraf22 <androa45@yahoo.com>
2023-08-17 17:43:44 -0700

    fixes 2

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new = add_dnodeint(h, n);
 		return (new);
 	}
-	if (idx < 0)
+	if ((int)idx < 0)
 		return (NULL);
 
 	temp2 = *h;
