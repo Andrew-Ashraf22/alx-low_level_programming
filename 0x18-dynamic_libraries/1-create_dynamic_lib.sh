79575c1cc78ec5a41a1324ab56c7dac43ee30a01
Andrew-Ashraf22 <androa45@yahoo.com>
2023-08-21 13:50:24 -0700

    task 1 fix

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -c *.c -o -fPIC
-gcc -shared -o liball.so *.o
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+gcc -c -fPIC *.c -o liball.o
+gcc -shared -o liball.so liball.o
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/alx-low_level_programming/0x18-dynamic_libraries/"
