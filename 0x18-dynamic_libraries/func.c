32409331c57da78e2ebb969648c6b9f4cb0a023c
Andrew-Ashraf22 <androa45@yahoo.com>
2023-08-21 07:27:45 -0700

    task 0

---
+++ 0x18-dynamic_libraries/func.c
@@ -0,0 +1,94 @@
+int _putchar(char c)
+{
+	return 1;
+}
+
+int _islower(int c)
+{
+	return 1;
+}
+
+int _isalpha(int c)
+{
+	return 1;
+}
+
+int _abs(int n)
+{
+	return 1;
+}
+
+int _isupper(int c)
+{
+	return 1;
+}
+
+int _isdigit(int c)
+{
+	return 1;
+}
+
+int _strlen(char *s)
+{
+	return 1;
+}
+
+void _puts(char *s)
+{
+	
+}
+
+char *_strcpy(char *dest, char *src)
+{
+	return "c";
+}
+
+int _atoi(char *s)
+{
+	return 1;
+}
+
+char *_strcat(char *dest, char *src)
+{
+	return "c";
+}
+
+char *_strncat(char *dest, char *src, int n)
+{
+	return "c";
+}
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	return "c";
+}
+
+int _strcmp(char *s1, char *s2)
+{
+	return 1;
+}
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	return "c";
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return "c";
+}
+
+char *_strchr(char *s, char c)
+{
+	return "c";
+}
+
+unsigned int _strspn(char *s, char *accept)
+{
+	return 1;
+}
+
+char *_strpbrk(char *s, char *accept)
+{return "c";}
+char *_strstr(char *haystack, char *needle)
+{return "c";}
